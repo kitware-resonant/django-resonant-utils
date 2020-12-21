0c4640edd6b560a764bb5c5624bac56d87fcbf3c
Brian Helba <brian.helba@kitware.com>
2020-12-21 17:44:48 -0500

    Make ReadonlyInlineMixin.get_readonly_fields more robust

--- girder_utils/admin.py
+++ girder_utils/admin.py
@@ -8,10 +8,21 @@ class ReadonlyInlineMixin:
     extra = 0
 
     def get_readonly_fields(self, request, obj=None):
-        if self.fields is None:
-            return []
-        # Make all fields readonly
-        return self.fields
+        # Because BaseModelAdmin.get_fields calls BaseModelAdmin.get_readonly_fields, we cannot
+        # get the list of fields from BaseModelAdmin.get_fields without infinite recursion.
+        # So, basically reimplement BaseModelAdmin.get_fields here, and return the same fields,
+        # thus making all fields readonly.
+        if self.fields:
+            return self.fields
+        form = self._get_form_for_get_fields(request, obj)
+        # Include any fields explicitly specified
+        explicit_readonly_fields = self.readonly_fields
+        return [*form.base_fields, *explicit_readonly_fields]
+
+    def get_fields(self, request, obj=None):
+        # get_readonly_fields now includes fields from form.base_fields, so get_fields should not
+        # include it twice.
+        self.get_readonly_fields(request, obj)
 
     def has_add_permission(self, request, obj=None):
         return False
