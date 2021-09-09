23a614123e5121f03b4698e036c73945dfdd3b65
Brian Helba <brian.helba@kitware.com>
2021-09-08 20:45:20 -0400

    Add type annotations to getitem

--- girder_utils/templatetags.py
+++ girder_utils/templatetags.py
@@ -1,10 +1,16 @@
+from typing import Mapping, Optional, TypeVar
+
 from django import template
 
 register = template.Library()
 
 
-@register.filter()
-def getitem(value, arg):
+_KT = TypeVar('_KT')
+_VT_co = TypeVar('_VT_co', covariant=True)
+
+
+@register.filter
+def getitem(value: Mapping[_KT, _VT_co], arg: _KT) -> Optional[_VT_co]:
     """
     Retrieve value[arg] from a template, where arg can be a variable.
     """
