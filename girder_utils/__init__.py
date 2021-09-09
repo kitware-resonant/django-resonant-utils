97c78e7e546f3ebb54011599eafdb5d8e2b3f03b
Brian Helba <brian.helba@kitware.com>
2021-09-08 22:23:10 -0400

    Create a Django app

--- girder_utils/__init__.py
+++ girder_utils/__init__.py
@@ -1,7 +1,12 @@
 from importlib.metadata import PackageNotFoundError, version
 
+import django
+
 try:
     __version__ = version('django-girder-utils')
 except PackageNotFoundError:
     # package is not installed
     pass
+
+if django.VERSION < (3, 2):
+    default_app_config = 'girder_utils.apps.GirderUtilsConfig'
