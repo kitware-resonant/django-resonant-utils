1796071abdc971e173b218c9ccabc653347006de
Brian Helba <brian.helba@kitware.com>
2022-07-13 17:44:36 -0400

    Fix some type checking issues

--- girder_utils/files.py
+++ girder_utils/files.py
@@ -30,10 +30,14 @@ def field_file_to_local_path(field_file: FieldFile) -> Generator[Path, None, Non
         if type(file_obj) is File:
             # When file_obj is an actual File, (typically backed by FileSystemStorage),
             # it is already at a stable path on disk.
+            if file_obj.name is None:
+                raise Exception('The File object "field_file.file" does not have a "name".')
             yield Path(file_obj.name)
         else:
             # When file_obj is actually a subclass of File, it only provides a Python
             # file-like object API. So, it must be copied to a stable path.
+            if field_file.name is None:
+                raise Exception('The FieldFile object "field_file" does not have a "name".')
             field_file_basename = PurePath(field_file.name).name
             with tempfile.NamedTemporaryFile('wb', suffix=field_file_basename) as dest_stream:
                 shutil.copyfileobj(file_obj, dest_stream)
